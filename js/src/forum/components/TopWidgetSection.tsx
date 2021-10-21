32b35918e0e11d4be6bba049379d9d16af1092d0
SychO9 <sychocouldy@gmail.com>
2021-10-21 18:44:18 +0100

    feat: Small screen improvements & misc fixes

--- js/src/forum/components/TopWidgetSection.tsx
+++ js/src/forum/components/TopWidgetSection.tsx
@@ -1,12 +1,37 @@
 import * as Mithril from 'mithril';
 import Component from 'flarum/common/Component';
+import Stream from 'flarum/common/utils/Stream';
+import classList from 'flarum/common/utils/classList';
 
 import sortWidgets from '../../common/utils/sortWidgets';
 
 export default class TopWidgetSection extends Component {
+  scrollEnd: Stream;
+
+  oninit(vnode): void {
+    super.oninit(vnode);
+
+    this.scrollEnd = Stream(false);
+  }
+
   view(): Mithril.Children {
+    const isSmallScreen = ['phone', 'tablet'].includes(app.screen());
+
     return (
-      <div className="AfruxWidgets-topWidgetSection AfruxWidgets-WidgetSection">
+      <div
+        className={classList({
+          'AfruxWidgets-topWidgetSection AfruxWidgets-WidgetSection': true,
+          'AfruxWidgets-WidgetSection--endScroll': isSmallScreen && this.scrollEnd(),
+        })}
+        onscroll={(e) => {
+          if (isSmallScreen) {
+            this.scrollEnd(false);
+
+            if (e.target.offsetWidth + e.target.scrollLeft >= e.target.scrollWidth) {
+              this.scrollEnd(true);
+            }
+          }
+        }}>
         {sortWidgets(app.widgets.get('top')).map((widget) => widget.component.component({ state: widget.state }))}
       </div>
     );
