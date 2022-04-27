282f66fd4ead98e860b5a893a3853b3072f70926
Goh Wee Liem <weeliem@outlook.com>
2022-04-27 14:45:56 +0800

    Adding in BottomWidgetSection
    Adding in BottomWidgetSection into compat.ts

--- js/src/forum/compat.ts
+++ js/src/forum/compat.ts
@@ -4,10 +4,12 @@ import EndWidgetSection from './components/EndWidgetSection';
 import StartTopWidgetSection from './components/StartTopWidgetSection';
 import StartBottomWidgetSection from './components/StartBottomWidgetSection';
 import TopWidgetSection from './components/TopWidgetSection';
+import BottomWidgetSection from './components/BottomWidgetSection';
 
 export default Object.assign(compat, {
   'extensions/afrux-forum-widgets-core/forum/components/EndWidgetSection': EndWidgetSection,
   'extensions/afrux-forum-widgets-core/forum/components/StartTopWidgetSection': StartTopWidgetSection,
   'extensions/afrux-forum-widgets-core/forum/components/StartBottomWidgetSection': StartBottomWidgetSection,
   'extensions/afrux-forum-widgets-core/forum/components/TopWidgetSection': TopWidgetSection,
+  'extensions/afrux-forum-widgets-core/forum/components/BottomWidgetSection': BottomWidgetSection,
 });
